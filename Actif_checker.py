2a455ee011c79262a47a8c4509a7338b0994a35c
Loic Masson <lomasson@k2r3p2.42mulhouse.fr>
2023-07-25 19:49:30 +0200

    retrograde/ promot

--- Actif_checker.py
+++ Actif_checker.py
@@ -1,8 +1,6 @@
 import requests
 import os
 
-API = 0
-
 ID_CLAN     = "208QLOJJV"
 FILE_DATA   = "./data.txt"
 FILE_API    = "./api.txt"
@@ -17,40 +15,51 @@ if not API:
     API = input("Entre ton API (https://developer.clashofclans.com/): ")
 
 #Récuperation des données
-session = requests.Session()
-session.headers.update({'Authorization': 'Bearer ' + API})
-response = session.get("https://api.clashofclans.com/v1/clans/%23" + ID_CLAN + "/capitalraidseasons?limit=1")
-data = response.json()
+try:
+
+    session = requests.Session()
+    session.headers.update({'Authorization': 'Bearer ' + API})
+    data = session.get("https://api.clashofclans.com/v1/clans/%23" + ID_CLAN + "/capitalraidseasons?limit=1").json()
+    all_members_name = session.get("https://api.clashofclans.com/v1/clans/%23" + ID_CLAN + "/members").json()
+except:
+    print("Echec de la récuperation des données")
 
 #Traitement
 try:
-    content_file = []
-    list_items = ""
     if (os.path.exists(FILE_DATA)):
         with open(FILE_DATA, "r") as fichier:
-            content_file = fichier.read()
-        content_file.split('\n')
-        # print(content_file[:-1])
+            old_war_members = fichier.read().split('\n')
+
+
     # PROMOTION
+    print("\n\t*****PROMOTION*****")
     for items in data['items'][0]['members']:
-        actual = items['name']
-        for past in content_file.split('\n'):
-            print(past)
-            print(actual)
-            if acutal == past:
-                print(actual + " a besoin d'une promotion")
+        for past in old_war_members:
+            if items['name'] == past:
+                print(items['name'] + "      \tà besoin d'une promotion.")
                 continue
+
+
+    # ECRITURE DES ACTIFS DE LA SEMAINE DANS LE FICHIER
     with open(FILE_DATA, "w") as fichier:
         for items in data['items'][0]['members']:
             fichier.write(items['name'] + '\n')
+            old_war_members.append(items['name'])
 
-    # ensemble = list(set(list_items) | set(content_file))
-    # Écrire le contenu dans le fichier
-        # members_war_1 = items['members']
-    # members_war_2 = data['items'][1]
-        # for member in items['name']:
-    # member_names__war_2 = [member['name'] for member in members_war_2]
-        # print(member)
-    # print(members_war_2)
+
+    #RETROGRADATION
+    print("\n\t*****RETROGRADATION*****")
+    retrogradation_list = []
+    for member in all_members_name['items']:
+        member = member['name']
+        retrogradation_list.append(member)
+        for actif in old_war_members:
+            if not actif:
+                continue
+            if member == actif:
+                retrogradation_list.remove(member)
+                break
+    for member in retrogradation_list:
+        print(member)
 except:
-    print("Echec de la récuperation des données")
+    print("Echec du traitement des données")
