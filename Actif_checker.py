eda5a8e18b82c7678f3e3c4d950ec39dc85652f9
lomasson <97454276+lomasson@users.noreply.github.com>
2023-12-27 10:56:37 +0000

    fix: unpomute admin/CoLeader

--- Actif_checker.py
+++ Actif_checker.py
@@ -4,28 +4,28 @@ import os
 ID_CLAN     = "208QLOJJV"
 FILE_DATA   = "./data.txt"
 FILE_API    = "./api.txt"
-#API
+# me #JJ2YGY08
+API         = ""
 try:
     myapi_file = open(FILE_API)
     API = myapi_file.read()
     myapi_file.close()
 except FileNotFoundError:
     print("Le fichier api.txt n'existe pas.")
+
 if not API:
     API = input("Entre ton API (https://developer.clashofclans.com/): ")
 
-#Récuperation des données
-try:
 
+try:
+#Récuperation des données
     session = requests.Session()
     session.headers.update({'Authorization': 'Bearer ' + API})
     data = session.get("https://api.clashofclans.com/v1/clans/%23" + ID_CLAN + "/capitalraidseasons?limit=1").json()
-    all_members_name = session.get("https://api.clashofclans.com/v1/clans/%23" + ID_CLAN + "/members").json()
-except:
-    print("Echec de la récuperation des données")
+    all_members_name = session.get("https://api.clashofclans.com/v1/clans/%23" + ID_CLAN + "/members").json()['items']
 
 #Traitement
-try:
+    old_war_members = ""
     if (os.path.exists(FILE_DATA)):
         with open(FILE_DATA, "r") as fichier:
             old_war_members = fichier.read().split('\n')
@@ -33,12 +33,12 @@ try:
 
     # PROMOTION
     print("\n\t*****PROMOTION*****")
-    for items in data['items'][0]['members']:
-        for past in old_war_members:
-            if items['name'] == past:
-                print(items['name'] + "      \tà besoin d'une promotion.")
-                continue
-
+    for member_info in all_members_name:
+        # Check if the member is not a CoLeader or admin
+        if member_info['role'] not in ['coLeader', 'admin']:
+            member_name = member_info['name']
+            if member_name in old_war_members:
+                print(f"{member_name}")
 
     # ECRITURE DES ACTIFS DE LA SEMAINE DANS LE FICHIER
     with open(FILE_DATA, "w") as fichier:
@@ -49,17 +49,16 @@ try:
 
     #RETROGRADATION
     print("\n\t*****RETROGRADATION*****")
-    retrogradation_list = []
-    for member in all_members_name['items']:
-        member = member['name']
-        retrogradation_list.append(member)
-        for actif in old_war_members:
-            if not actif:
-                continue
-            if member == actif:
-                retrogradation_list.remove(member)
-                break
-    for member in retrogradation_list:
-        print(member)
-except:
-    print("Echec du traitement des données")
+    retrogradation_list = [member['name'] for member in all_members_name]
+
+    for actif in old_war_members:
+        if not actif:
+            continue
+        if actif in retrogradation_list:
+            retrogradation_list.remove(actif)
+
+    for member_name in retrogradation_list:
+        print(f"{member_name}")
+
+except Exception as e:
+    print("Echec de la récuperation des données:", str(e))
