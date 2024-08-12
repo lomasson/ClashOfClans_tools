937d8019d6520466f9e1316cb4e1b5aeb1cccf4e
snaker <loic.masson67@hotmail.fr>
2024-08-12 14:55:18 +0100

    fix: adjust weekly compare

--- Actif_checker.py
+++ Actif_checker.py
@@ -19,13 +19,14 @@ if not API:
 
 
 try:
-#Récuperation des données
+
+# Récuperation des données
     session = requests.Session()
     session.headers.update({'Authorization': 'Bearer ' + API})
     data = session.get("https://api.clashofclans.com/v1/clans/%23" + ID_CLAN + "/capitalraidseasons?limit=1").json()['items'][0]
     all_members_name = session.get("https://api.clashofclans.com/v1/clans/%23" + ID_CLAN + "/members").json()['items']
 
-#Traitement
+# Traitement
     old_war_members = []
     if (os.path.exists(FILE_DATA)):
         with open(FILE_DATA, "r") as fichier:
@@ -35,35 +36,29 @@ try:
     end_time = datetime.datetime.strptime(data['endTime'], "%Y%m%dT%H%M%S.%fZ")
     formatted_end_time = end_time.strftime("%d/%m/%Y")
 
+    retrogradation_list = [member['name'] for member in all_members_name]
+
     # PROMOTION
+    acutal_war = [member['name'] for member in data['members']]
     print("\n\t*****PROMOTION*****")
     for member_info in all_members_name:
-        # Check if the member is not a CoLeader or admin
-        if member_info['role'] not in ['coLeader', 'admin']:
-            member_name = member_info['name']
-            if member_name in old_war_members:
-                print(f"{member_name}")
+        member_name = member_info['name']
+        if member_name in old_war_members or member_name in acutal_war:
+            if member_name in old_war_members and member_name in acutal_war:
+                if member_info['role'] not in ['coLeader', 'leader', 'admin']:
+                    print(f"{member_name}")
+            retrogradation_list.remove(member_name)
+
+    # RETROGRADATION
+    print("\n\t*****RETROGRADATION*****")
+    for member_name in retrogradation_list:
+        print(f"{member_name}")
 
     # ECRITURE DES ACTIFS DE LA SEMAINE DANS LE FICHIER
     with open(FILE_DATA, "w") as file:
         file.write(formatted_end_time + '\n')
         for items in data['members']:
             file.write(items['name'] + '\n')
-            old_war_members.append(items['name'])
-
-
-    #RETROGRADATION
-    print("\n\t*****RETROGRADATION*****")
-    retrogradation_list = [member['name'] for member in all_members_name]
-
-    for actif in old_war_members:
-        if not actif:
-            continue
-        if actif in retrogradation_list:
-            retrogradation_list.remove(actif)
-
-    for member_name in retrogradation_list:
-        print(f"{member_name}")
 
 except Exception as e:
-    print("Echec de la récuperation des données:", str(e))
+    print("Echec de la récuperation des données:", e.__str__)
